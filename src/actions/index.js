1d9fb31d4f0673ed7714eabd227089129f007a84
Sergejs Dementjevs <dozornik88@gmail.com>
2020-11-29 18:57:32 +0100

    Modal added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import history from "../history";
 import {
 	SIGN_IN,
 	SIGN_OUT,
@@ -22,9 +23,11 @@ export const signOut = () => {
 	};
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-	const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+	const { userId } = getState().auth;
+	const response = await streams.post("/streams", { ...formValues, userId });
 	dispatch({ type: CREATE_STREAM, payload: response.data });
+	history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -39,12 +42,14 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-	const response = await streams.put(`/streams/${id}`, formValues);
+	const response = await streams.patch(`/streams/${id}`, formValues);
 	dispatch({ type: EDIT_STREAM, payload: response.data });
+	history.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
 	await streams.delete(`/streams/${id}`);
 
 	dispatch({ type: DELETE_STREAM, payload: id });
+	history.push("/");
 };
